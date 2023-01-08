b1f4d2603f46f2a0d8ef3323217c98d6bf679694
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2023-01-08 20:47:22 +0100

    delete client

--- front-end/src/Component/client.jsx
+++ front-end/src/Component/client.jsx
@@ -53,7 +53,7 @@ export default function Client(){
     )
   }
 
-
+  //for add client
   const handleChangeAdd = (e) => {
     setDisplay({...display, [e.target.name]: e.target.value})
     setErrors({...errors, [e.target.name]: ""})
@@ -68,14 +68,16 @@ export default function Client(){
     }
     if(!display.cin){
       setErrors({...errors, cin: "cin is required"})
+      return
     }
     if(!display.tel){
       setErrors({...errors, tel: "tel is required"})
+      return
     }
     if(!regixTel.test(display.tel)){
       setErrors({...errors, tel: "tel is not valid"})
+      return
     }
-    if(display.fullname && display.cin && display.tel){
       await axios.post('http://localhost:5050/api/client/addClient',display)
       .then((response)=>{
         console.log(response.data.msg)   
@@ -86,8 +88,21 @@ export default function Client(){
         console.log(err)
         setErrors({...errors, cin: err.response.data})
       })  
-    }  
+    
   }
+
+  //for delete client
+  function deleteClient(id){
+    axios.delete(`http://localhost:5050/api/client/deleteClient/${id}`)
+    .then((response)=>{
+      console.log(response.data)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
+
 
 return(
   <> 
@@ -141,6 +156,7 @@ return(
                           </svg>
                         </button>
                     </button>
+                    <button type="button" onClick={() => deleteClient(client_._id)}>
                     <a x-data="{ tooltip: 'Delete' }" href="">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" className="h-6 w-6" x-tooltip="tooltip">
@@ -148,6 +164,7 @@ return(
                           d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                       </svg>
                     </a>
+                    </button>
                   </div>
                 </td>
               </tr>
